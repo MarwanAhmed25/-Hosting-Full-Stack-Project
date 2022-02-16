b2b6c0d6cd496df68e9c72279c6c62e648e38316
MarwanAhmed25 <marwan4125881@gmail.com>
2022-02-16 14:37:05 +0200

    d

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -11,18 +11,3 @@ export const sequelize = new Sequelize({
   dialect: "postgres",
   storage: ":memory:",
 });
-
-/*
-
-pass: master password : postgres
-user: master username : postgres
-name: instance : postgres
-port: default : 5432
-host: endpoint : 
-
-
-export const sequelize = new Sequelize(
-  "postgres://postgres:postgres@database-1.c6eyc2xkmulu.us-east-1.rds.amazonaws.com:5432/database-1"
-);
-
-*/
